207ae764c6dea5cab18aeca3045b23dd23b571ac
Sean Davis <sean.dev.davis@gmail.com>
2025-09-12 20:34:25 -0500

    categories are displaying

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('Jeopardy')
+const cats = document.getElementById("categories");
+const li = document.createElement("li");
+
+let dataCategory = [];
+
+// const getSubjects = async () => {
+//   axios
+//     .get("https://rithm-jeopardy.herokuapp.com/api/categories?count=6")
+//     .then((res) => {
+//       data = res?.data;
+//       // console.log(data)
+//       data?.forEach(el => {
+//         // creates a div for each category
+//         const ul = document.createElement('ul')
+//         ul.textContent = `${el?.title.toUpperCase()}`
+//         cats.appendChild(ul)
+//         ul.classList.add('subject')
+//       })
+//     }).catch((err) => {
+//       console.log('Could not get categories', err)
+//     })
+// };
+
+axios
+  .get("https://rithm-jeopardy.herokuapp.com/api/categories?count=6")
+  .then((response) => {
+    dataCategory = response?.data;
+    console.log(dataCategory);
+    //       // console.log(data)
+    dataCategory?.forEach((el) => {
+      // creates a div for each category
+      const ul = document.createElement("ul");
+      ul.textContent = `${el?.title.toUpperCase()}`;
+      cats.appendChild(ul);
+      ul.classList.add("subject");
+    });
+  })
+  .catch((error) => {
+    console.log("Error fetching data:", error);
+  });
+
+  console.log(dataCategory)
